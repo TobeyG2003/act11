2c20d297c4790d4e494e9c9113ccf000ce7ff0c3
Tobey <TobeyG2003@gmail.com>
2025-10-20 18:22:30 -0400

    setup of db (unpopulated)

--- lib/database_setup.dart
+++ lib/database_setup.dart
@@ -9,96 +9,47 @@ class DatabaseHelper {
   static const columnId = '_id';
   static const columnName = 'name';
   static const columnAge = 'age';
-  late Database _db;
+  late Database foldersdb;
+  late Database cardsdb;
 // this opens the database (and creates it if it doesn't exist)
   Future<void> init() async {
     final documentsDirectory = await getApplicationDocumentsDirectory();
-    final path = join(documentsDirectory.path, _databaseName);
-    _db = await openDatabase(
+    final path = join(documentsDirectory.path, 'myfolders.db');
+    final path2 = join(documentsDirectory.path, 'mycards.db');
+    foldersdb = await openDatabase(
       path,
       version: _databaseVersion,
-      onCreate: _onCreate,
+      onCreate: _onCreateFolders,
+    );
+    cardsdb = await openDatabase(
+      path2,
+      version: _databaseVersion,
+      onCreate: _onCreateCards,
     );
   }
 
 // SQL code to create the database table
-  Future _onCreate(Database db, int version) async {
+  Future _onCreateFolders(Database db, int version) async {
     await db.execute('''
 CREATE TABLE $table (
-$columnId INTEGER PRIMARY KEY,
-$columnName TEXT NOT NULL,
-$columnAge INTEGER NOT NULL
+'id' INTEGER PRIMARY KEY,
+'name' TEXT,
+'previewImage' TEXT,
+'createdAt' TEXT
 )
 ''');
   }
-
-// Helper methods
-// Inserts a row in the database where each key in the
-//Map is a column name
-// and the value is the column value. The return value
-//is the id of the
-// inserted row.
-  Future<int> insert(Map<String, dynamic> row) async {
-    return await _db.insert(table, row);
-  }
-
-// All of the rows are returned as a list of maps, where each map is
-// a key-value list of columns.
-  Future<List<Map<String, dynamic>>> queryAllRows() async {
-    return await _db.query(table);
-  }
-
-// All of the methods (insert, query, update, delete) can also be done using
-// raw SQL commands. This method uses a raw query to give the row count.
-  Future<int> queryRowCount() async {
-    final results = await _db.rawQuery('SELECT COUNT(*) FROM $table');
-    return Sqflite.firstIntValue(results) ?? 0;
-  }
-
-   Future<Map<String, dynamic>?> queryid(int id) async {
-    final results = await _db.query(
-      table,
-      where: '$columnId = ?',
-      whereArgs: [id],
-    );
-    if (results.isNotEmpty) {
-      return results.first;
-    }
-    else {
-      return null;
-    }
-    /*return await _db.delete(
-      table,
-      where: '$columnId = ?',
-      whereArgs: [id],
-    );*/
-  }
-
-// We are assuming here that the id column in the map is set. The other
-// column values will be used to update the row.
-  Future<int> update(Map<String, dynamic> row) async {
-    int id = row[columnId];
-    return await _db.update(
-      table,
-      row,
-      where: '$columnId = ?',
-      whereArgs: [id],
-    );
-  }
-
-// Deletes the row specified by the id. The number of affected rows is
-// returned. This should be 1 as long as the row exists.
-  Future<int> delete(int id) async {
-    return await _db.delete(
-      table,
-      where: '$columnId = ?',
-      whereArgs: [id],
-    );
-  }
-
-  Future<int> deleteall(int id) async {
-    return await _db.delete(
-      table,
-    );
+  Future _onCreateCards(Database db, int version) async {
+    await db.execute('''
+CREATE TABLE $table (
+'id' INTEGER PRIMARY KEY,
+'name' TEXT,
+'suite' TEXT,
+'imageURl' TEXT,
+'imageBytes' TEXT,
+'folderId' INTEGER, FOREIGN KEY,
+'createdAt' TEXT
+)
+''');
   }
 }
