99211c820f8c57bfab2fea2ecc85593d0fda74db
Tobey <TobeyG2003@gmail.com>
2025-10-20 19:26:14 -0400

    adds folder and card classes

--- lib/main.dart
+++ lib/main.dart
@@ -46,3 +46,75 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class Folder {
+  int? id;
+  String name;
+  String? previewImage;
+  DateTime createdAt;
+
+  Folder({this.id, required this.name, this.previewImage, required this.createdAt});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'previewImage': previewImage,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  factory Folder.fromMap(Map<String, dynamic> map) {
+    return Folder(
+      id: map['id'],
+      name: map['name'],
+      previewImage: map['previewImage'],
+      createdAt: DateTime.parse(map['createdAt']),
+    );
+  }
+}
+
+class Card {
+  int? id;
+  String name;
+  String suit;
+  String imageUrl;
+  String? imageBytes; // base64 string
+  int? folderId;
+  DateTime createdAt;
+
+  Card({
+    this.id,
+    required this.name,
+    required this.suit,
+    required this.imageUrl,
+    this.imageBytes,
+    this.folderId,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'suit': suit,
+      'imageUrl': imageUrl,
+      'imageBytes': imageBytes,
+      'folderId': folderId,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  factory Card.fromMap(Map<String, dynamic> map) {
+    return Card(
+      id: map['id'],
+      name: map['name'],
+      suit: map['suit'],
+      imageUrl: map['imageUrl'],
+      imageBytes: map['imageBytes'],
+      folderId: map['folderId'],
+      createdAt: DateTime.parse(map['createdAt']),
+    );
+  }
+}
+
